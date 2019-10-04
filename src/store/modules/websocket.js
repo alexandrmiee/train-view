041b98cacac592a79ce971bfd455a9e6f209d557
Alexandr Lukovnikov <alexandr.lukovnikov@whenspeak.ru>
2019-10-04 23:44:20 +0300

    add ws server connection

--- src/store/modules/websocket.js
+++ src/store/modules/websocket.js
@@ -1,11 +1,42 @@
 let ws = null;
 let connectionTimeout = null;
 
-const reconnect = (server, timeout) => {
+const stopReconnect = () => {
+  clearInterval(connectionTimeout);
+  connectionTimeout = null;
+};
+
+// const close = () => {
+//   if (ws) ws.close();
+//   stopReconnect();
+// };
+
+const reconnect = (server, timeout, commit) => {
+  if (ws) ws.close();
   ws = null;
+  stopReconnect();
   connectionTimeout = setInterval(() => {
     /**TODO: subscribe */
     ws = new WebSocket(server);
+    ws.onopen = () => {
+      commit(`updateProp`, { prop: "connected", value: true });
+      stopReconnect();
+    };
+
+    ws.onmessage = data => {
+      console.log(data.data);
+      commit(`updateProp`, { prop: "message", value: JSON.parse(data.data) });
+    };
+    ws.onerror = () => {
+      commit(`updateProp`, { prop: "connected", value: false });
+      reconnect(server, timeout, commit);
+      ws.close();
+    };
+
+    ws.onclose = () => {
+      commit(`updateProp`, { prop: "connected", value: false });
+      reconnect(server, timeout, commit);
+    };
   }, timeout);
 };
 
@@ -17,31 +48,21 @@ export default {
   },
   actions: {
     sendMessage(__store, data) {
-      if (ws) ws.send(data);
+      if (ws) {
+        ws.send(JSON.stringify(data));
+        console.log("try to send data");
+      }
       return new Error("not connected");
     },
 
     connect({ commit }, { server, timeout }) {
       if (ws) ws.close();
-      ws = new WebSocket(server);
+      reconnect(server, timeout, commit);
+    },
 
-      ws.onopen = () => {
-        commit(`updateProp`, { prop: "connected", value: true });
-        clearInterval(connectionTimeout);
-        connectionTimeout = null;
-      };
-
-      ws.onmessage = data =>
-        commit(`updateProp`, { prop: "message", value: data });
-      ws.onerror = () => {
-        commit(`updateProp`, { prop: "connected", value: false });
-        reconnect(server, timeout);
-      };
-
-      ws.onclose = () => {
-        commit(`updateProp`, { prop: "connected", value: false });
-        reconnect(server, timeout);
-      };
+    close() {
+      if (ws) ws.close();
+      stopReconnect();
     }
   },
   mutations: {
--- src/views/Control.vue
+++ src/views/Control.vue
@@ -1,14 +1,32 @@
 <template>
-  <chart-bar style="position: relative; height:40vh; width:80vw" />
+  <div>
+    <b-button block @click="startTest" :disabled="!connected">Старт</b-button>
+    <chart-bar style="position: relative; height:40vh; width:80vw" />
+  </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import ChartBar from "@/components/ChartBar.vue";
+import { BButton } from "bootstrap-vue";
 
 export default {
   name: "Control",
   components: {
-    ChartBar
+    ChartBar,
+    BButton
+  },
+  computed: {
+    ...mapState("websocket", ["message", "connected"])
+  },
+  mounted() {
+    this.connect({ server: "ws://127.0.0.1:3000/ws", timeout: 1000 });
+  },
+  methods: {
+    ...mapActions("websocket", ["sendMessage", "connect"]),
+    startTest() {
+      this.sendMessage({ command: "start" });
+    }
   }
 };
 </script>
